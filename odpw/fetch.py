9f50be1f41f01ff75ec0e092835d798c6c2754c9
Sebastian Neumaier <sebastian.neumaier@wu.ac.at>
2015-07-27 13:50:50 +0200

    socrata fetching

--- odpw/fetch.py
+++ odpw/fetch.py
@@ -6,6 +6,8 @@ from odpw.analysers.quality.analysers.completeness import CompletenessAnalyser
 from odpw.analysers.quality.analysers.contactability import ContactabilityAnalyser
 from odpw.analysers.quality.analysers.openness import OpennessAnalyser
 from odpw.analysers.quality.analysers.opquast import OPQuastAnalyser
+from odpw.portal_processor import CKAN, Socrata
+
 __author__ = 'jumbrich'
 
 
@@ -13,16 +15,13 @@ from datetime import datetime
 from multiprocessing.process import Process
 from time import sleep
 from odpw.head import HeadProcess
-from urlparse import urlparse
 
 
-from odpw.db.models import Portal, Dataset, PortalMetaData, Resource
+from odpw.db.models import Portal, PortalMetaData
 
-import ckanapi
 import odpw.util as util
 from odpw.util import getSnapshot,getExceptionCode,ErrorHandler as eh
 
-from odpw.timer import Timer
 import argparse
 
 import logging
@@ -33,84 +32,6 @@ configure(logger_factory=LoggerFactory())
 log = get_logger()
 
 
-import random
-import time
-
-
-def generateFetchDatasetIter(Portal, sn):
-    
-    api = ckanapi.RemoteCKAN(Portal.apiurl, get_only=True)
-    start=0
-    rows=1000000
-    
-    processed=set([])
-    
-    while True:
-        response = api.action.package_search(rows=rows, start=start)
-        #print Portal.apiurl, start, rows, len(processed)
-        datasets = response["results"] if response else None
-        if datasets:
-            rows = len(datasets) if start==0 else rows
-            start+=rows
-            for datasetJSON in datasets:
-                datasetID = datasetJSON['name']
-                
-                if datasetID not in processed:
-                    data = datasetJSON
-                    
-                    d = Dataset(snapshot=sn,portal=Portal.id, dataset=datasetID, data=data)
-                    d.status=200
-                    
-                    processed.add(datasetID)
-                        
-                    if len(processed) % 1000 == 0:
-                        log.info("ProgressDSFetch", pid=Portal.id, processed=len(processed))
-                    
-                    yield d
-                
-        else:
-            break
-    try:
-        package_list, status = util.getPackageList(Portal.apiurl)
-        total=len(package_list)
-    
-        for entity in package_list:
-            #WAIT between two consecutive GET requests
-            if entity not in processed:
-                processed.add(d.dataset)
-                 
-                time.sleep(random.uniform(0.5, 1))
-                log.debug("GETMetaData", pid=Portal.id, did=entity)
-                with Timer(key="fetchDS("+Portal.id+")") as t, Timer(key="fetchDS") as t1:
-                    #fetchDataset(entity, stats, dbm, sn)
-                    props={
-                           'status':-1,
-                           'data':None,
-                           'exception':None
-                           }
-                    try:
-                        resp = api.action.package_show(id=entity)
-                        data = resp
-                        util.extras_to_dict(data)
-                        props['data']=data
-                        props['status']=200
-                    except Exception as e:
-                        eh.handleError(log,'FetchDataset', exception=e,pid=Portal.id, did=entity,
-                           exc_info=True)
-                        props['status']=util.getExceptionCode(e)
-                        props['exception']=str(type(e))+":"+str(e.message)
-                 
-                    d = Dataset(snapshot=sn,portal=Portal.id, dataset=entity, **props)
-                    processed.add(d.dataset)
-                         
-                    if len(processed) % 1000 == 0:
-                        log.info("ProgressDSFetch", pid=Portal.id, processed=len(processed))
-                     
-                    yield d
-                    
-    except Exception as e:
-        if len(processed)==0:
-            raise e
 
 def fetching(obj):
     Portal = obj['portal']
@@ -120,11 +41,8 @@ def fetching(obj):
 
     dbm.engine.dispose()
     
-    
     try:
-        
         log.info("START Fetching", pid=Portal.id, sn=sn, fullfetch=fullfetch)
-
         pmd = dbm.getPortalMetaData(portalID=Portal.id, snapshot=sn)
         if not pmd:
             pmd = PortalMetaData(portal=Portal.id, snapshot=sn)
@@ -132,54 +50,56 @@ def fetching(obj):
         pmd.fetchstart()
         dbm.updatePortalMetaData(pmd)
 
-        ae = AnalyseEngine()
-    
-        ae.add(MD5DatasetAnalyser())
-        ae.add(DatasetCount())
-        ae.add(ResourceInDS(withDistinct=True))
-        ae.add(ResourceInserter(dbm))
-        ae.add(DatasetStatusCount())
-        ae.add(ResourceInDSAge())
-        ae.add(DatasetAge())
-        ae.add(KeyAnalyser())
-        ae.add(FormatCount())
-    
-        qae = QualityAnalyseEngine()
-        qae.add(CompletenessAnalyser())
-        qae.add(ContactabilityAnalyser())
-        qae.add(OpennessAnalyser())
-        qae.add(OPQuastAnalyser())
-    
         main = AnalyseEngine()
-        main.add(ae)
-        main.add(qae)
-        
-        Portal.latest_snapshot=sn
-    
-        iter = generateFetchDatasetIter(Portal, sn)
-        main.process_all(iter)
-    
+        if Portal.software == 'CKAN':
+            ae = AnalyseEngine()
+
+            ae.add(MD5DatasetAnalyser())
+            ae.add(DatasetCount())
+            ae.add(ResourceInDS(withDistinct=True))
+            ae.add(ResourceInserter(dbm))
+            ae.add(DatasetStatusCount())
+            ae.add(ResourceInDSAge())
+            ae.add(DatasetAge())
+            ae.add(KeyAnalyser())
+            ae.add(FormatCount())
+
+            qae = QualityAnalyseEngine()
+            qae.add(CompletenessAnalyser())
+            qae.add(ContactabilityAnalyser())
+            qae.add(OpennessAnalyser())
+            qae.add(OPQuastAnalyser())
+
+            main.add(ae)
+            main.add(qae)
+
+            processor = CKAN(main)
+        elif Portal.software == 'Socrata':
+            ae = AnalyseEngine()
+            qae = QualityAnalyseEngine()
+            main.add(ae)
+            main.add(qae)
+            processor = Socrata(main)
+        else:
+            raise NotImplementedError(Portal.software + ' is not implemented')
+
+        processor.fetching(Portal, sn)
+
         for ae in main.getAnalysers():
             for analyser in ae:
                 analyser.update(pmd)
                 analyser.update(Portal)
-       
+
         dbm.updatePortalMetaData(pmd)
 
-        
         Portal.datasets= ae.getAnalyser('ds').getResult()['count']
         Portal.resources= ae.getAnalyser('res').getResult()['count']
     except Exception as exc:
         Portal.status=getExceptionCode(exc)
         Portal.exception=str(type(exc))+":"+str(exc.message)
-        
+
     dbm.updatePortal(Portal)
-    
-    
-
-
     log.info("END Fetching", pid=Portal.id, sn=sn, fullfetch=fullfetch)
-
 
 
 
