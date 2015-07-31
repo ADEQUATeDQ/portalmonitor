1a0be01a752567ed5ef22e61820c33374542df50
umbrich <umbrich@ai.wu.ac.at>
2015-07-31 16:30:26 +0200

    added missing modules

---
+++ odpw/analysers/pmd_analysers.py
@@ -0,0 +1,61 @@
+'''
+Created on Jul 30, 2015
+
+@author: jumbrich
+'''
+from odpw.analysers import Analyser
+
+
+
+class PMDActivityAnalyser(Analyser):
+    
+    def __init__(self):
+        self.stats=[]
+        self.stats_key=['fetch_done','fetch_failed','fetch_running','fetch_missing',
+                        'head_missing','head_done',
+                        'quality_done']
+        self.sum={}
+        for k in self.stats_key:
+            self.sum[k]=0
+    
+    def analyse_PortalMetaData(self, pmd):
+        #print pmd
+        
+        stats={ 'pid':pmd.portal_id, 'snapshot':pmd.snapshot,'fetch_error':''}
+        for k in self.stats_key:
+            stats[k]=None
+        
+        if pmd.fetch_stats:
+            stats['fetch_done']=all(pmd.fetch_stats.get(k) for k in ['fetch_start', 'fetch_end']) and pmd.fetch_stats.get('exception')==None
+            stats['fetch_failed'] = all(pmd.fetch_stats.get(k) for k in ['fetch_start', 'exception'])
+            if stats['fetch_failed']:
+                
+                stats['fetch_error']= pmd.fetch_stats['exception'].split(":")[0].replace("<class '","").replace("'>","").replace("<type '","")
+                
+            stats['fetch_running'] = not stats['fetch_done'] and not stats['fetch_failed'] and  pmd.fetch_stats.get('fetch_start',None) != None
+        else:
+            stats['fetch_missing']=True
+            
+        if pmd.res_stats:
+            res_total = pmd.res_stats.get('total',-1)
+            res_unique = pmd.res_stats.get('unique',-1)
+            
+            stats['head_done']=bool(pmd.res_stats['respCodes']) if 'respCodes' in pmd.res_stats else False
+            stats['head_missing']= not stats['head_done']  
+        else:
+            stats['head_missing']=True
+            
+        #if pmd.qa_stats:
+            
+        self.stats.append(stats)
+        
+        for k in self.stats_key:
+            if stats[k]:
+                self.sum[k] += 1    
+        
+    
+    def done(self):
+        pass
+        
+    def getResult(self):
+        return {'rows':self.stats, 'summary':self.sum, 'columns':self.stats_key+['pid','snapshot','fetch_error'],'summary_columns':self.stats_key}
---
+++ odpw/utils/report.py
@@ -0,0 +1,105 @@
+from odpw.reporting.reporters import SystemActivityReporter, ReporterEngine, SoftWareDistReporter,\
+    ISO3DistReporter, SnapshotsPerPortalReporter
+
+__author__ = 'jumbrich'
+
+from odpw.utils.util import getSnapshot
+
+import structlog
+log =structlog.get_logger()
+
+
+def name():
+    return 'Report'
+def help():
+    return "Generate various reports"
+
+def setupCLI(pa):
+    
+    pa.add_argument("-sn","--snapshot",  help='what snapshot is it', dest='snapshot')
+    pa.add_argument("-i","--ignore",  help='Force to use current date as snapshot', dest='ignore', action='store_true')
+    
+    
+    
+    tasks = pa.add_argument_group("system reports")
+    tasks.add_argument("--overview",  help='generate the system overview report', dest='sysover', action='store_true')
+    tasks.add_argument("--activity",  help='generate the system activity report', dest='sysactivity', action='store_true')
+    
+    tasks = pa.add_argument_group("portal reports")
+    #tasks.add_argument("--apiurl",  help='Portal apiurl ', dest='apiurl')
+    tasks.add_argument("--pid",  help='Portal id ', dest='portal_id')
+    tasks.add_argument("--pgen",  help='Portal overview report', dest='pgen', action='store_true')
+    tasks.add_argument("--pdetail",  help='Portal detail report', dest='pdetail', action='store_true')
+    tasks.add_argument("--pquality",  help='Portal quality report', dest='pquality', action='store_true')
+    tasks.add_argument("--pactivity",  help='Portal activity report', dest='pactivity', action='store_true')
+    tasks.add_argument("--pevolution",  help='Portal evolution report', dest='pevolution', action='store_true')
+    
+    
+    out = pa.add_argument_group("Output")
+    out.add_argument("--out",  help='outputfolder to write the reports', dest='outdir')
+    out.add_argument("--csv",  help='generate the system activity report', dest='csv', action='store_true')
+    out.add_argument("--ui",  help='generate the system activity report', dest='ui', action='store_true')
+    
+def cli(args,dbm):
+    
+    
+    #for pmd in dbm.getLatestPortalMetaDatas():
+    #    print pmd
+    outdir= args.outdir
+    if not outdir and any([args.csv]):
+        print "No output dir "
+        return
+        
+    
+    if args.sysover:
+        sys_or = ReporterEngine([SoftWareDistReporter(dbm),
+                                 ISO3DistReporter(dbm)])
+        sys_or.run()
+    
+        output(sys_or,args)
+        
+    if any([args.pevolution,
+            args.pgen,
+            args.pdetail,
+            args.pquality, 
+            args.pactivity,
+            args.sysactivity ]):
+        sn = getSnapshot(args)
+        if not sn:
+            print "No snapshot specified"
+            return
+             
+    
+    if args.sysactivity:
+        sys_act_rep = ReporterEngine([SystemActivityReporter(dbm,sn)])
+        sys_act_rep.run()
+        
+        output(sys_act_rep,args)
+    
+    if any([args.pevolution,args.pgen,args.pdetail,args.pquality, args.pactivity ]) and not any([  args.portal_id]):
+        print "Portal URL or ID is missing"
+        return 
+    
+    if args.pgen:    
+        rep = ReporterEngine([SnapshotsPerPortalReporter(dbm,portalID=args.portal_id)])
+        rep.run()
+        
+        output(rep,args)
+        
+    if args.pdetail:
+        pass
+    if args.pquality:
+        pass
+    if args.pevolution:
+        pass
+    if args.pactivity:
+        pass
+    
+def output(rE, args):
+    
+    rE.clireport()
+    
+    if args.csv:
+        rE.csvreport(args.outdir)
+    if args.ui:
+        print rE.uireport()
