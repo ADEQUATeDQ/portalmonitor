95e7120839b5a57e128899747f7e9360132c5839
Sebastian Neumaier <sebastian.neumaier@wu.ac.at>
2015-08-10 15:59:37 +0200

    bugfix

--- odpw/__init__.py
+++ odpw/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'jumbrich'
 
-#import urllib3.contrib.pyopenssl
-#urllib3.contrib.pyopenssl.inject_into_urllib3()
+import urllib3.contrib.pyopenssl
+urllib3.contrib.pyopenssl.inject_into_urllib3()
 
 #import urllib3
 #urllib3.disable_warnings()
