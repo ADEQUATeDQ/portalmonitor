bebdbb20f8693f603aea85bdcd498da71bfa7ba0
umbrich <umbrich@ai.wu.ac.at>
2015-08-13 10:11:54 +0200

    fixed dcat script

--- odpw/analysers/fetching.py
+++ odpw/analysers/fetching.py
@@ -494,7 +494,7 @@ class UsageAnalyser(Analyser):
         for i in [ 'extra', 'core']:
             a = np.array(self.keysperDS[i])
             
-            if len(self.keysperDS[i]) != 0:
+            if len(keys[i].keys()) != 0:
                 a = a/( len(keys[i].keys()) *1.0)
             self.quality[i] = a.mean() if len(self.keysperDS[i]) != 0 else 0
             
