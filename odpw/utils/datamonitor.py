e6eb21482f007f2b3e9a24942416468ee6c8b83d
Sebastian Neumaier <sebastian.neumaier@wu.ac.at>
2015-11-25 18:23:39 +0100

    insert urls in datamonitor

--- odpw/utils/datamonitor.py
+++ odpw/utils/datamonitor.py
@@ -17,89 +17,62 @@ log = get_logger()
 
 
 def help():
-    return "INsert URIs into the datamonitor"
+    return "Insert URIs into the datamonitor"
 def name():
     return 'DM'
 
 def setupCLI(pa):
-    pa.add_argument('-s','--schedule',action='store_true', dest='schedule')
-    pa.add_argument("-sn","--snapshot",  help='what snapshot is it', dest='snapshot')
-    pa.add_argument("-i","--ignore",  help='Force to use current date as snapshot', dest='ignore', action='store_true')
+    pa.add_argument("-f","--file",  help='file containing csv urls', dest='file')
 
-def cli(args,dbm):
-    
-        sn = getSnapshot(args)
-        if not sn:
-            return
+def cli(args, dbm):
+
+    experiment = 'csvengine'
+    nextCrawl = datetime(year=2015, month=11, day=25, hour=20)
+    print 'Crawl time', nextCrawl
+
+    dm_dbm = DMManager(db='datamonitor', host="datamonitor-data.ai.wu.ac.at", port=5432, password='d4tamonitor', user='datamonitor')
+
+    with open(args.file) as f:
+        for url in f:
+            dm_dbm.upsert(url, experiment, nextCrawl, 0)
+
+    #psql --host datamonitor-data.ai.wu.ac.at -U datamonitor -W datamonitor
+        #sn = getSnapshot(args)
+        #if not sn:
+        #    return
+
+        #today = datetime.now()
+        #start = today + timedelta((6 - today.weekday()) % 7)
+        #start = start.replace(hour=0, minute=0, second=0, microsecond=0)
+        #end = start + timedelta(days=5)
+        #log.info("Scheduling URIs", start=start, end=end)
+
         
-        today = datetime.now()
-        start = today + timedelta((6 - today.weekday()) % 7)
-        start = start.replace(hour=0, minute=0, second=0)
-        end = start + timedelta(5)
-        log.info("Scheduling URIs", start=start, end=end)
-        
-        experiment = 'odwu'
-        
-        dm_dbm = DMManager(db='datamonitor', host="137.208.51.23", port=5432, password='p0stgres', user='postgres')
-        
-        urls={}
+        #urls={}
         # get list of URLs for the snapshot 
         #for res in dbm.getResources(snapshot=sn, status=-1):
         #    # print str(res['status'])[0]
         #    urls[res['url']]={'experiment':experiment, 'freq':10800}
         
-        nct = today - timedelta(1)
-        print nct
-        for resS in dm_dbm.getOldSchedule(nct):
-            urls[ resS['uri'] ]={'experiment':resS['experiment'], 'freq':resS['frequency']}
+        #nct = today - timedelta(1)
+        #print nct
+        #for resS in dm_dbm.getOldSchedule(nct):
+        #    urls[ resS['uri'] ]={'experiment':resS['experiment'], 'freq':resS['frequency']}
             
-        t = len(urls)
-        steps = t/10
+        #t = len(urls)
+        #steps = t/10
         
-        print t
+        #print t
         
-        nextCrawl = start
-        c=0
-        for url in urls:
-            c+=1
-            dm_dbm.upsert(url, urls[url]['experiment'], nextCrawl, urls[url]['freq'])
+        #nextCrawl = start
+        #c=0
+        #for url in urls:
+        #    c+=1
             
-            if c%steps==0:
-                progressIndicator(c, t)
+        #    if c%steps==0:
+        #        progressIndicator(c, t)
                 
-            nextCrawl += timedelta(hours=1)
-            if nextCrawl >= end:
-                nextCrawl = start
-        
-        
-if __name__ == '__main__':
-    
-    dm_dbm = DMManager(db='datamonitor', host="137.208.51.23", port=5432, password='p0stgres', user='postgres')
-    
-    dm_dbm.upsert('http://example', 'text', '','')
-    
+        #    nextCrawl += timedelta(hours=1)
+        #    if nextCrawl >= end:
+        #        nextCrawl = start
 
-    def datespan(startDate, endDate, delta=timedelta(days=1)):
-        currentDate = startDate
-        
-        
-        while currentDate < endDate:
-            yield currentDate
-            currentDate += delta
-    
-    today = datetime.now()
-    mon = today + timedelta( (6-today.weekday()) % 7 )
-    
-    print mon
-    mon= mon.replace(hour=0, minute=0, second=0)
-    end=mon+timedelta(7)
-    
-    
-    
-#     dm_dbm= PostGRESManager(db='datamonitor', host="137.208.51.23", port=5432, password=None, user='postgres')
-#     for res in dm_dbm.selectQuery("Select * from schedule Limit 10"):
-#         print res
-#         
-#     dm_dbm= PostGRESManager(db='portalwatch', host="137.208.51.23", port=5433, password='0pwu', user='opwu')
-#     for res in dm_dbm.selectQuery("Select * from resources WHERE snapshot='2015-25'Limit 10"):
-#         print res['status'],res['url']
