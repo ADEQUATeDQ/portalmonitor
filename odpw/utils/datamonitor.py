05e6808eaf632728982c5677fc10ad08703b2da5
Sebastian Neumaier <sebastian.neumaier@wu.ac.at>
2015-11-26 11:25:52 +0100

    url validation

--- odpw/utils/datamonitor.py
+++ odpw/utils/datamonitor.py
@@ -4,8 +4,7 @@ __author__ = 'jumbrich'
 
 import sys
 from odpw.db.models import Portal
-from django.core.validators import URLValidator
-from django.core.exceptions import ValidationError
+import validators
 from odpw.utils.util import ErrorHandler as eh, getSnapshot, progressIndicator
 from datetime import date, datetime, timedelta
 
@@ -24,23 +23,26 @@ def name():
 
 def setupCLI(pa):
     pa.add_argument("-f","--file",  help='file containing csv urls', dest='file')
+    pa.add_argument("-d","--crawldate",  help='crawldate', dest='date')
 
 def cli(args, dbm):
 
     experiment = 'csvengine'
-    nextCrawl = datetime(year=2015, month=11, day=26, hour=11)
-    print 'Crawl time', nextCrawl
+    if not args.crawldate:
+        nextCrawl = datetime(year=2015, month=11, day=26, hour=12)
+        print 'Crawl time', nextCrawl
+    else:
+        nextCrawl = datetime.strptime(args.crawldate, '%Y-%m-%d %H:%M:%S')
 
     dm_dbm = DMManager(db='datamonitor', host="datamonitor-data.ai.wu.ac.at", port=5432, password='d4tamonitor', user='datamonitor')
-    val = URLValidator(verify_exists=False)
 
     with open(args.file) as f:
         for u in f:
             url = u.strip()
             try:
-                val(url)
+                validators.url(url)
                 dm_dbm.upsert(url, experiment, nextCrawl, 0)
-            except ValidationError, e:
+            except Exception, e:
                 print e
 
     #psql --host datamonitor-data.ai.wu.ac.at -U datamonitor -W datamonitor
