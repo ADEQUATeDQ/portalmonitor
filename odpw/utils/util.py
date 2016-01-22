9b1bb8e5681f02bf979f5139abacb29167845deb
umbrich <umbrich@ai.wu.ac.at>
2016-01-22 17:15:54 +0100

    added timestamp to head

--- odpw/utils/util.py
+++ odpw/utils/util.py
@@ -688,6 +688,7 @@ def head(url, redirects=0, props=None):
             props['status']=None
             props['header']=None
             props['exception']=None
+            props['timestamp'] = datetime.now()
         
         headResp = requests.head(url=url,timeout=(0.5, 1.0), allow_redirects=True)#con, read -timeout
         header_dict = dict((k.lower(), v) for k, v in dict(headResp.headers).iteritems())
