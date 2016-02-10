b131937449d5fb07ca93d8febc83836c49524397
jumbrich <jueumb@gmail.com>
2016-02-10 17:32:33 +0100

    added dataset changes

---
+++ odpw/analysers/dataset_analysers.py
@@ -0,0 +1,21 @@
+'''
+Created on Feb 10, 2016
+
+@author: jumbrich
+'''
+from freshness import json_compare
+from odpw.analysers.core import ElementCountAnalyser
+
+
+
+class DatasetChangeCountAnalyser(ElementCountAnalyser):
+    def __init__(self, datasets):
+        super(DatasetChangeCountAnalyser, self).__init__()
+        self.datasets=datasets
+        
+    def analyse_Dataset(self, dataset):
+        if dataset.data and dataset.id in self.datasets:
+            diffs = json_compare.jsondiff(self.datasets[dataset.id].data, dataset.data)
+            for mode, selector, changes in diffs:
+                k= mode+'_'+"_".join([str(s) for s in selector])
+                self.add(k)
