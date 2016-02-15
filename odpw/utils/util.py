95116aa94837467b81b7278964a6d31f3c59ac11
Sebastian Neumaier <sebastian.neumaier@wu.ac.at>
2016-02-15 11:30:27 +0100

    head bugfix

--- odpw/utils/util.py
+++ odpw/utils/util.py
@@ -690,7 +690,7 @@ def head(url, redirects=0, props=None):
             props['exception']=None
             props['timestamp'] = datetime.now()
         
-        headResp = requests.head(url=url,timeout=(0.5, 1.0), allow_redirects=True)#con, read -timeout
+        headResp = requests.head(url=url,timeout=(3, 9), allow_redirects=True)#con, read -timeout
         header_dict = dict((k.lower(), v) for k, v in dict(headResp.headers).iteritems())
         
         if 'content-type' in header_dict:
