03e21efc676aa825e7d4c2adc6a67dbe6724537b
neumaier <sebastian.neumaier@wu.ac.at>
2017-05-29 14:19:48 +0200

    config update

--- dockerize/portalwatch_services/datafetch/datafetch.sh
+++ dockerize/portalwatch_services/datafetch/datafetch.sh
@@ -8,7 +8,7 @@ echo "Writting logs to $LOGS for week $week"
 
 LOGF=data-fetch_$week
 
-SCRIPT="odpw -c $ADEQUATE/odpw.conf DataFetch "
+SCRIPT="odpw -c $ADEQUATE/portalmonitor.conf DataFetch "
 cmd="$SCRIPT 1>> $LOGS/$LOGF.out 2> $LOGS/$LOGF.err"
 echo $cmd
 eval $cmd
