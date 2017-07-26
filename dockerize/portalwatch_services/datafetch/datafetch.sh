a993779676a584d031896966d49b296a87da4c0f
neumaier <sebastian.neumaier@wu.ac.at>
2017-07-26 11:35:44 +0200

    datafetch script

--- dockerize/portalwatch_services/datafetch/datafetch.sh
+++ dockerize/portalwatch_services/datafetch/datafetch.sh
@@ -3,27 +3,29 @@
 #initially kill running processes
 pkill  -f Fetch
 
+DATE=`date +%Y-%m-%d`
 week=`date +"%y%V"`
 echo "Writting logs to $LOGS for week $week"
 
-LOGF=data-fetch_data-gv-at_$week
-SCRIPT="odpw -c $ADEQUATE/portalmonitor.conf DataFetch -p data_gv_at "
-cmd="$SCRIPT 1>> $LOGS/$LOGF.out 2> $LOGS/$LOGF.err"
-echo $cmd
-eval $cmd
-gzip $LOGS/$LOGF.*
+PORTALS=("data_gv_at" "www_opendataportal_at")
 
-LOGF=data-fetch_odp_$week
-SCRIPT="odpw -c $ADEQUATE/portalmonitor.conf DataFetch -p www_opendataportal_at "
-cmd="$SCRIPT 1>> $LOGS/$LOGF.out 2> $LOGS/$LOGF.err"
-echo $cmd
-eval $cmd
-gzip $LOGS/$LOGF.*
+for pName in "${PORTALS[@]}"
+do
+    LOGF=data-fetch_"$pName"_$week
+    SCRIPT="odpw -c $ADEQUATE/portalmonitor.conf DataFetch -p $pName "
+    cmd="$SCRIPT 1>> $LOGS/$LOGF.out 2> $LOGS/$LOGF.err"
+    echo $cmd
+    #eval $cmd
+    #gzip $LOGS/$LOGF.*
+done
 
 LOGF=git-store_$week
 SCRIPT="odpw -c $ADEQUATE/portalmonitor.conf GitDataStore"
 cmd="$SCRIPT 1>> $LOGS/$LOGF.out 2> $LOGS/$LOGF.err"
 echo $cmd
-eval $cmd
+#eval $cmd
+#gzip $LOGS/$LOGF.*
 
-gzip $LOGS/$LOGF.*
+P=$(printf %s\\n "${PORTALS[@]}"|sed 's/["\]/\\&/g;s/.*/"&"/;1s/^/[/;$s/$/]/;$!s/$/,/')
+
+printf '{"portals":%s,"snapshot":"%s","date":"%s"}\n' "$P" "$week" "$DATE" > /logs/fetched_portals.conf
